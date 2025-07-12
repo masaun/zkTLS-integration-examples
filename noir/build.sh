9ce59781e448c73ebf2a74a604af1281549cf0a0
masaun <masaun@protonmail.com>
2025-07-12 22:06:37 +0900

    [circuit - build.sh]: Add the Solidity Verifier generation commands

--- noir/build.sh
+++ noir/build.sh
@@ -14,10 +14,20 @@ if ! nargo compile; then
 fi
 
 echo "Gate count:"
-bb gates -b target/zktls_integrations.json | jq '.functions[0].circuit_size'
+bb gates -b target/zktls_integrations-$VERSION.json | jq '.functions[0].circuit_size'
 
+echo "Generating vk.json to ./target/zktls_integrations/zktls_integrations-$VERSION..."
+node -e "const fs = require('fs'); fs.writeFileSync('./target/zktls_integrations-$VERSION/vk.json', JSON.stringify(Array.from(Uint8Array.from(fs.readFileSync('./target/vk/vk')))));"
 
-# [TODO]: 
+echo "Generate a Solidity Verifier contract from the vkey..."
+bb write_solidity_verifier -k ./target/vk/vk -o ./target/Verifier.sol
+
+echo "Copy a Solidity Verifier contract-generated (Verifier.sol) into the ./contracts/src/circuits/circuit-for-zkemail-2048-bit-dkim/honk-verifier directory"
+cp ./target/Verifier.sol ../contracts/circuits/honk-verifier
+
+echo "Rename the Verifier.sol with the honk_vk.sol in the ./contracts/circuits/ultra-verifier directory"
+mv ../contracts/circuits/honk-verifier/Verifier.sol ../contracts/circuits/honk-verifier/honk_vk.sol
+
 
 
 echo "Done" 
