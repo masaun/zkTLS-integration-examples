8eb124aa8c41fb3d153c8d6f0129c236f92a7ce4
masaun <masaun@protonmail.com>
2025-07-14 11:00:23 +0900

    [SC test]: Move the existing SC test for the zkTLS with OKX DEX API into the ./test/circuits/zktls-integration-with-okx-dex-api

--- test/circuits/zktls-integration-with-okx-dex-api/ZkTLSIntegrationsProofVerifier.test.ts
+++ test/circuits/zktls-integration-with-okx-dex-api/ZkTLSIntegrationsProofVerifier.test.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
 
-import { main, getData } from "../../scripts/OKX-DEX-API/okx-dex-sdk-integrations/okx-dex-sdk-api-request-sample"; 
+import { main, getData } from "../../../scripts/OKX-DEX-API/okx-dex-sdk-integrations/okx-dex-sdk-api-request-sample"; 
 
 
 it("proves and verifies on-chain", async () => {
