9384933087d540d48732f85c3e6c2d58bf7ea365
masaun <masaun@protonmail.com>
2025-07-14 17:18:58 +0900

    [scripts]: Remove the example-requests-in-ts/js

--- scripts/OKX-DEX-API/example-requests-in-js/runningScript_example-post-request.sh
+++
@@ -1,7 +0,0 @@
-echo "Load the environment variables from the .env file..."
-source ./scripts/OKX-DEX-API/.env
-#source ../../../.env
-#. ./.env
-
-echo "Run the script file of the example-post-request.js"
-node ./scripts/OKX-DEX-API/example-requests/example-post-request.js
